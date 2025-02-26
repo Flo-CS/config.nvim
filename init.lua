a6eb3beab98cffeb6dd88b1c7040293bb4e8e72b
Flo-CS <53081078+Flo-CS@users.noreply.github.com>
2025-02-27 00:07:38 +0100

    move cursor when inserting line

--- init.lua
+++ init.lua
@@ -80,7 +80,7 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
-vim.keymap.set('n', '<CR>', 'O<Esc>')
+vim.keymap.set('n', '<CR>', 'O<Esc>j')
 vim.keymap.set('n', ']<CR>', 'o<Esc>')
 vim.keymap.set('n', '[<CR>', 'O<Esc>')
 
