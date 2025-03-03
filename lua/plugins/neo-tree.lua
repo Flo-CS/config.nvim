1d814ec5350e5dc74fe3e9587731b26c8b80e5cf
Flo-CS <53081078+Flo-CS@users.noreply.github.com>
2025-03-03 14:10:09 +0100

    update neo-tree

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -12,13 +12,18 @@ return {
   cmd = 'Neotree',
   keys = {
     { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
+    { '<leader>e', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
   },
   opts = {
     filesystem = {
+      hijack_netrw_behavior = 'open_current',
       window = {
-        mappings = {
-          ['\\'] = 'close_window',
-        },
+        position = 'left',
+      },
+    },
+    window = {
+      mappings = {
+        ['\\'] = 'close_window',
       },
     },
   },
